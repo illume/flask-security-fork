65b4b2f6888a04fbbd7a40717ed69e4f4bfcf13b
krahser <aldo.vizcaino87@gmail.com>
2017-03-01 23:47:50 +0800

    add a primary_key

--- tests/conftest.py
+++ tests/conftest.py
@@ -221,6 +221,7 @@ def sqlalchemy_session_datastore(request, app, tmpdir):
 
     class RolesUsers(Base):
         __tablename__ = 'roles_users',
+        id = Column(Integer(), primary_key=True)
         user_id = Column('user_id', Integer(), ForeignKey('user.id'))
         role_id = Column('role_id', Integer(), ForeignKey('role.id'))
 

575574c762ba0636e48621d1972c86511bf6a62e
krahser <aldo.vizcaino87@gmail.com>
2017-03-02 09:13:28 +0800

    add modification as illume says

--- flask_security/datastore.py
+++ flask_security/datastore.py
@@ -273,6 +273,24 @@ class SQLAlchemySessionUserDatastore(SQLAlchemyUserDatastore,
                                          user_model,
                                          role_model)
 
+    def get_user(self, identifier):
+        if self._is_numeric(identifier):
+            return self.session.query(self.user_model).get(identifier)
+        for attr in get_identity_attributes():
+            query = getattr(self.user_model, attr).ilike(identifier)
+            rv = self.session.query(self.user_model).filter(query).first()
+            if rv is not None:
+                return rv
+
+
+    def find_user(self, **kwargs):
+        return self.session.query(self.user_model).filter_by(**kwargs).first()
+
+
+    def find_role(self, role):
+        return self.session.query(self.user_model).filter_by(name=role).first()
+
+
 
 class MongoEngineUserDatastore(MongoEngineDatastore, UserDatastore):
     """A MongoEngine datastore implementation for Flask-Security that assumes
